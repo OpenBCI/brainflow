5d5c95c04277c04b5f327acdc34a363b8ad19e01
Andrey Parfenov <a1994ndrey@gmail.com>
2019-07-06 23:18:59 +0200

    add push to dropbox
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

--- tools/dropbox_uploader.py
+++ tools/dropbox_uploader.py
@@ -1,5 +1,8 @@
-import dropbox
 import argparse
+import os
+
+import dropbox
+import glob
 
 
 class TransferData:
@@ -9,19 +12,26 @@ class TransferData:
 
     def upload_file (self, file_from, file_to):
         dbx = dropbox.Dropbox (self.access_token)
-        with open (file_from, 'rb') as f:
-            dbx.files_upload (f.read (), file_to)
+        # good enought for our task
+        if file_from.endswith ('.h') or file_from.endswith ('.cmake'):
+            with open (file_from, 'r') as f:
+                dbx.files_upload (f.read (), file_to)
+        else:
+            with open (file_from, 'rb') as f:
+                dbx.files_upload (f.read (), file_to)
 
 def main ():
 
     parser = argparse.ArgumentParser ()
     parser.add_argument ('--token', type = str, help  = 'access token', required = True)
-    parser.add_argument ('--local-file', type = str, help  = 'local file to send', required = True)
-    parser.add_argument ('--remote-file', type = str, help = 'remote file location', required = True)
+    parser.add_argument ('--local-files', type = str, help  = 'local file to send', required = True)
+    parser.add_argument ('--remote-dir', type = str, help = 'remote file location', required = True)
     args = parser.parse_args ()
 
     transferData = TransferData (args.token)
-    transferData.upload_file (args.local_file, args.remote_file)
+    for file in glob.glob (args.local_files):
+        remote_file = args.remote_dir + '/' + os.path.split (file)[1]
+        transferData.upload_file (file, remote_file)
 
 if __name__ == '__main__':
     main ()
