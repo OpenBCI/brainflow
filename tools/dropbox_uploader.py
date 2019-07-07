02edb90038848ff104d50abb7f85bd45d0426dd6
Andrey Parfenov <a1994ndrey@gmail.com>
2019-07-08 01:35:43 +0200

    temp
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

--- tools/dropbox_uploader.py
+++ tools/dropbox_uploader.py
@@ -28,6 +28,7 @@ class TransferData:
             with open (file_from, 'rb') as f:
                 dbx.files_upload (f.read (), file_to)
 
+
 def main ():
     logging.basicConfig (level = logging.INFO, format = '%(asctime)s [%(levelname)s] %(message)s')
 
@@ -37,13 +38,13 @@ def main ():
     parser.add_argument ('--remote-dir', type = str, help = 'remote file location', required = True)
     args = parser.parse_args ()
 
-    transferData = TransferData (args.token)
+    transfer_data = TransferData (args.token)
     logging.info ('remote dir %s' % args.remote_dir)
     logging.info ('files: %s' % '\n'.join(glob.glob (args.local_files)))
     for file in glob.glob (args.local_files):
         remote_file = args.remote_dir + '/' + os.path.split (file)[1]
         try:
-            transferData.upload_file (file, remote_file)
+            transfer_data.upload_file (file, remote_file)
         except Exception as e:
             logging.warning (str (e))
 
