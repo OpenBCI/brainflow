bc0c00fc8a10ce02a4889443121e3a9ddb9f42e4
Andrey Parfenov <a1994ndrey@gmail.com>
2019-07-27 00:26:57 +0200

    wip novaxr
    Signed-off-by: Andrey Parfenov <a1994ndrey@gmail.com>

---
+++ src/utils/socket.cpp
@@ -0,0 +1,85 @@
+#include "socket.h"
+
+
+///////////////////////////////
+/////////// WINDOWS ///////////
+//////////////////////////////
+#ifdef _WIN32
+
+#pragma comment(lib, "Ws2_32.lib")
+#pragma comment(lib, "Mswsock.lib")
+#pragma comment(lib, "AdvApi32.lib")
+
+Socket::Socket (const char *ip_addr, int port)
+{
+    strcpy (this->ip_addr, ip_addr);
+    this->port = port;
+    connect_socket = INVALID_SOCKET;
+}
+
+int Socket::connect ()
+{
+    WSADATA wsadata;
+    int res = WSAStartup (MAKEWORD (2, 2), &wsadata);
+    if (res != 0)
+    {
+        return (int)SocketReturnCodes::WSA_STARTUP_ERROR;
+    }
+    struct sockaddr_in socket_addr;
+    socket_addr.sin_family = AF_INET;
+    socket_addr.sin_port = htons (port);
+    socket_addr.sin_addr.s_addr = inet_addr (ip_addr);
+
+    connect_socket = socket (AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (connect_socket == INVALID_SOCKET)
+    {
+        return (int)SocketReturnCodes::CREATE_SOCKET_ERROR;
+    }
+
+    // ensure that library will not hang in blocking recv/send call
+    DWORD timeout = 3000;
+    setsockopt (connect_socket, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof (timeout));
+    setsockopt (connect_socket, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout, sizeof (timeout));
+
+    res = ::connect (connect_socket, (sockaddr *)&socket_addr, sizeof (socket_addr));
+    if (res == SOCKET_ERROR)
+    {
+        return (int)SocketReturnCodes::CONNECT_ERROR;
+    }
+
+    return (int)SocketReturnCodes::STATUS_OK;
+}
+
+int Socket::send (void *data, int size)
+{
+    int res = ::send (connect_socket, (char *)data, size, 0);
+    if (res == SOCKET_ERROR)
+    {
+        return -1;
+    }
+    return res;
+}
+
+int Socket::recv (void *data, int size)
+{
+    int res = ::recv (connect_socket, (char *)data, size, 0);
+    if (res == SOCKET_ERROR)
+    {
+        return -1;
+    }
+    return res;
+}
+
+void Socket::close ()
+{
+    closesocket (connect_socket);
+    connect_socket = INVALID_SOCKET;
+    WSACleanup ();
+}
+
+///////////////////////////////
+//////////// UNIX /////////////
+///////////////////////////////
+#else
+
+#endif
