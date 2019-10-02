05ae812c3ab3ecebfd99631ce4989f511cfb1cac
“Fan1117” <macfan1995@gmail.com>
2019-10-02 17:25:16 -0400

    one prepare, one start, 5 threads to collect data

---
+++ tests/csharp/multithread/test/ThreadCollector.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using brainflow;
+
+namespace test
+{
+    class ThreadCollector
+    {
+        public int sleep_ms = 2000;
+        public BoardShim board_shim;
+        public int board_id;
+        public bool keep_alive = true;
+        public DateTime thread_start_time;
+
+        public ThreadCollector(BoardShim board, string[] args)
+        {
+            thread_start_time = DateTime.Now;
+            board_shim = board;
+            board_id = Int32.Parse(args[0]);
+        }
+
+        public void collect_data()
+        {
+            while (keep_alive)
+            {
+                System.Threading.Thread.Sleep(sleep_ms);
+                //Console.WriteLine("data count: {0}", board_shim.get_board_data_count());
+            }
+            double[,] unprocessed_data = board_shim.get_board_data();
+            // check serialization
+            DataHandler dh = new DataHandler(board_id, data_from_board: unprocessed_data);
+            dh.save_csv(thread_start_time.ToString("HH-mm-ss") + "before_processing.csv");
+            dh = new DataHandler(board_id, csv_file: thread_start_time.ToString("HH-mm-ss") + "before_processing.csv");
+            dh.save_csv(thread_start_time.ToString("HH-mm-ss") + "before_preprocessing2.csv");
+            // check preprocessing
+            dh.remove_dc_offset();
+            dh.bandpass(1.0, 50.0);
+            dh.save_csv(thread_start_time.ToString("HH-mm-ss") + "after_preprocessing.csv");
+        }
+
+        public void stop_collecting()
+        {
+            keep_alive = false;
+        }
+    }
+}
---
+++ tests/csharp/multithread/test/get_board_data.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using brainflow;
+
+namespace test
+{
+    class GetBoardData
+    {
+        static void Main(string[] args)
+        {
+            int thread_number = 5;
+            System.Threading.Thread[] thread_array = new System.Threading.Thread[thread_number];
+            ThreadCollector[] thread_collector_array = new ThreadCollector[thread_number];
+
+            BoardShim.enable_dev_board_logger();
+            BoardShim board_shim;
+            if (args.Length == 2)
+                board_shim = new BoardShim(Int32.Parse(args[0]), args[1]);
+            else
+                board_shim = new BoardShim(Int32.Parse(args[0]), null);
+
+            // initialize thread_collectors
+            for (int i = 0; i < thread_number; i++)
+            {
+                System.Threading.Thread.Sleep(1000);
+                thread_collector_array[i] = new ThreadCollector(board_shim, args);
+            }
+
+            // prepare and start streaming
+            board_shim.prepare_session();
+            Console.WriteLine("Session is ready");
+
+            board_shim.start_stream(3600);
+            Console.WriteLine("Started");
+            System.Threading.Thread.Sleep(5000);
+
+            // collect data in threads
+            for (int i = 0; i < thread_number; i++)
+            {
+                System.Threading.Thread.Sleep(500);
+                thread_array[i] = new System.Threading.Thread(thread_collector_array[i].collect_data);
+                thread_array[i].Start();
+                Console.WriteLine("Thread " + i.ToString() + " Start!");
+            }
+
+            // wait for 10s
+            System.Threading.Thread.Sleep(10000);
+
+            // stop collecting data in threads
+            for (int i = 0; i < thread_number; i++)
+            {
+                System.Threading.Thread.Sleep(500);
+                thread_collector_array[i].keep_alive = false;
+                thread_array[i].Join();
+                Console.WriteLine("Thread " + i.ToString() + " stopped!");
+            }
+
+            // wait for 1s
+            System.Threading.Thread.Sleep(1000);
+
+            // stop streaming and release
+            board_shim.stop_stream();
+            Console.WriteLine("Stopped");
+
+            board_shim.release_session();
+            Console.WriteLine("Released");
+        }
+    }
+}
