b46d432f64034d0de7788ca203af1e9d85b993c7
“Fan1117” <macfan1995@gmail.com>
2019-10-03 00:47:30 -0400

    add reference

--- tests/csharp/multithread/multithread/ThreadCollector.cs
+++ tests/csharp/multithread/multithread/ThreadCollector.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using brainflow;
+﻿using brainflow;
+using System;
 
 namespace multithread
 {
@@ -15,33 +11,33 @@ namespace multithread
         public bool keep_alive = true;
         public DateTime thread_start_time;
 
-        public ThreadCollector (BoardShim board, string[] args)
+        public ThreadCollector(BoardShim board, string[] args)
         {
             thread_start_time = DateTime.Now;
             board_shim = board;
-            board_id = Int32.Parse (args[0]);
+            board_id = Int32.Parse(args[0]);
         }
 
-        public void collect_data ()
+        public void collect_data()
         {
             while (keep_alive)
             {
-                System.Threading.Thread.Sleep (sleep_ms);
-                Console.WriteLine ("data count: {0}", board_shim.get_board_data_count ());
+                System.Threading.Thread.Sleep(sleep_ms);
+                Console.WriteLine("data count: {0}", board_shim.get_board_data_count());
             }
-            double[,] unprocessed_data = board_shim.get_board_data ();
+            double[,] unprocessed_data = board_shim.get_board_data();
             // check serialization
-            DataHandler dh = new DataHandler (board_id, data_from_board: unprocessed_data);
-            dh.save_csv (thread_start_time.ToString ("HH-mm-ss") + "before_processing.csv");
-            dh = new DataHandler (board_id, csv_file: thread_start_time.ToString ("HH-mm-ss") + "before_processing.csv");
-            dh.save_csv (thread_start_time.ToString ("HH-mm-ss") + "before_preprocessing2.csv");
+            DataHandler dh = new DataHandler(board_id, data_from_board: unprocessed_data);
+            dh.save_csv(thread_start_time.ToString("HH-mm-ss") + "before_processing.csv");
+            dh = new DataHandler(board_id, csv_file: thread_start_time.ToString("HH-mm-ss") + "before_processing.csv");
+            dh.save_csv(thread_start_time.ToString("HH-mm-ss") + "before_preprocessing2.csv");
             // check preprocessing
-            dh.remove_dc_offset ();
-            dh.bandpass (1.0, 50.0);
-            dh.save_csv (thread_start_time.ToString ("HH-mm-ss") + "after_preprocessing.csv");
+            dh.remove_dc_offset();
+            dh.bandpass(1.0, 50.0);
+            dh.save_csv(thread_start_time.ToString("HH-mm-ss") + "after_preprocessing.csv");
         }
 
-        public void stop_collecting ()
+        public void stop_collecting()
         {
             keep_alive = false;
         }
