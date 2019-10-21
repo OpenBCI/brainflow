61759fbd9dcbce0f802e0ff4093bf4f1b63f3290
Fan1117 <macfan1995@gmail.com>
2019-10-21 16:40:27 -0400

    fix comments

--- tests/csharp/multithread/test/ThreadCollector.cs
+++ tests/csharp/multithread/test/ThreadCollector.cs
@@ -15,11 +15,11 @@ namespace test
         public bool keep_alive = true;
         public DateTime thread_start_time;
 
-        public ThreadCollector(BoardShim board, string[] args)
+        public ThreadCollector(BoardShim board, int id)
         {
             thread_start_time = DateTime.Now;
             board_shim = board;
-            board_id = Int32.Parse(args[0]);
+            board_id = id;
         }
 
         public void collect_data()
@@ -28,17 +28,9 @@ namespace test
             {
                 System.Threading.Thread.Sleep(sleep_ms);
                 Console.WriteLine("data count: {0}", board_shim.get_board_data_count());
+                double[,] unprocessed_data = board_shim.get_current_board_data();
             }
-            double[,] unprocessed_data = board_shim.get_board_data();
-            // check serialization
-            DataHandler dh = new DataHandler(board_id, data_from_board: unprocessed_data);
-            dh.save_csv(thread_start_time.ToString("HH-mm-ss") + "before_processing.csv");
-            dh = new DataHandler(board_id, csv_file: thread_start_time.ToString("HH-mm-ss") + "before_processing.csv");
-            dh.save_csv(thread_start_time.ToString("HH-mm-ss") + "before_preprocessing2.csv");
-            // check preprocessing
-            dh.remove_dc_offset();
-            dh.bandpass(1.0, 50.0);
-            dh.save_csv(thread_start_time.ToString("HH-mm-ss") + "after_preprocessing.csv");
+            
         }
 
         public void stop_collecting()
--- tests/csharp/multithread/test/get_board_data.cs
+++ tests/csharp/multithread/test/get_board_data.cs
@@ -12,17 +12,15 @@ namespace test
             ThreadCollector[] thread_collector_array = new ThreadCollector[thread_number];
 
             BoardShim.enable_dev_board_logger();
-            BoardShim board_shim;
-            if (args.Length == 2)
-                board_shim = new BoardShim(Int32.Parse(args[0]), args[1]);
-            else
-                board_shim = new BoardShim(Int32.Parse(args[0]), null);
+            BrainFlowInputParams input_params = new BrainFlowInputParams();
+            int board_id = parse_args(args, input_params);
+            BoardShim board_shim = new BoardShim(board_id, input_params);
 
             // initialize thread_collectors
             for (int i = 0; i < thread_number; i++)
             {
                 System.Threading.Thread.Sleep(1000);
-                thread_collector_array[i] = new ThreadCollector(board_shim, args);
+                thread_collector_array[i] = new ThreadCollector(board_shim, board_id);
             }
 
             // prepare and start streaming
@@ -31,8 +29,7 @@ namespace test
 
             board_shim.start_stream(3600);
             Console.WriteLine("Started");
-            System.Threading.Thread.Sleep(5000);
-
+   
             // collect data in threads
             for (int i = 0; i < thread_number; i++)
             {
@@ -60,9 +57,50 @@ namespace test
             // stop streaming and release
             board_shim.stop_stream();
             Console.WriteLine("Stopped");
-
+            double[,] unprocessed_data = board_shim.get_current_board_data(50);
+            int[] eeg_channels = BoardShim.get_eeg_channels(board_id);
+            Console.WriteLine("Before processing:");
+            foreach (var index in eeg_channels)
+                Console.WriteLine("[{0}]", string.Join(", ", unprocessed_data.GetRow(index)));
             board_shim.release_session();
             Console.WriteLine("Released");
+        }
+        static int parse_args(string[] args, BrainFlowInputParams input_params)
+        {
+            int board_id = (int)BoardIds.SYNTHETIC_BOARD; //assume synthetic board by default
+            // use docs to get params for your specific board, e.g. set serial_port for Cyton
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].Equals("--ip-address"))
+                {
+                    input_params.ip_address = args[i + 1];
+                }
+                if (args[i].Equals("--mac-address"))
+                {
+                    input_params.mac_address = args[i + 1];
+                }
+                if (args[i].Equals("--serial-port"))
+                {
+                    input_params.serial_port = args[i + 1];
+                }
+                if (args[i].Equals("--other-info"))
+                {
+                    input_params.other_info = args[i + 1];
+                }
+                if (args[i].Equals("--ip-port"))
+                {
+                    input_params.ip_port = Convert.ToInt32(args[i + 1]);
+                }
+                if (args[i].Equals("--ip-protocol"))
+                {
+                    input_params.ip_protocol = Convert.ToInt32(args[i + 1]);
+                }
+                if (args[i].Equals("--board-id"))
+                {
+                    board_id = Convert.ToInt32(args[i + 1]);
+                }
+            }
+            return board_id;
         }
     }
 }
